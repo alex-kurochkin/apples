32afb571115c1491497e8dd67b294e55c7732343
alex <inspector@list.ru>
2020-02-06 01:13:05 +0600

    Frontend. Real list display

--- frontend/views/site/apples.php
+++ frontend/views/site/apples.php
@@ -1,11 +1,15 @@
 APPLES
+<hr />
+<a href="javascript:void(0);" id="createApples">Create apples</a>
 <div class="apples">
-    <table>
+    <table id="applesTable">
         <thead>
             <th>id</th>
             <th>Color</th>
+            <th>Created</th>
+            <th>Fall</th>
             <th>Fallen</th>
-            <th>Rotten</th>
+            <th>State</th>
             <th>Eat it</th>
             <th>Eaten %</th>
             <th>Drop it</th>
@@ -13,6 +17,13 @@ APPLES
         <tbody id="apples-list"></tbody>
     </table>
 </div>
+AccessToken: <?= Yii::$app->user->identity->access_token; ?>
 <script>
     let AccessToken = '<?= Yii::$app->user->identity->access_token; ?>';
 </script>
+<style>
+    table td:nth-child(2),
+    table td:nth-child(6) {
+        text-transform: capitalize;
+    }
+</style>
--- frontend/web/js/apples.js
+++ frontend/web/js/apples.js
@@ -1,28 +1,145 @@
-$(document).ready(function() {
+/**
+ * @param state
+ * @returns {string}
+ */
+const detectAppleState = function (state) {
+    'use strict';
+    switch (state) {
+        case 1:
+            return 'fresh';
+        case 0:
+            return 'rotten';
+        case null:
+            return 'unripe';
+    }
+
+    return '???';
+};
+
+/**
+ *
+ * @param fallenDT {Date}
+ * @param freshDuration {number}
+ * @returns {number} 1 - fresh, 0 - rotten
+ */
+const calculateFresh = function (fallenDT, freshDuration) {
+    'use strict';
+    let freshDurationSeconds = freshDuration * 60 * 60,
+        currentDateTime = new Date(),
+        dtDiff = currentDateTime.getTime() - fallenDT.getTime();
+
+    return freshDurationSeconds > dtDiff ? 1 : 0;
+};
+
+const AppleList = function () {
+    'use strict';
+    let applesList = $('#apples-list');
+    return {
+        load: function (apple) {
+            applesList.append('<tr id="' + apple.id + '">' +
+                '<td>' + apple.id + '</td>' +
+                '<td>' + apple.color + '</td>' +
+                '<td>' + apple.createdAt + '</td>' +
+                '<td><a href="javascript:void(0);" class="fallApple" data-id="' + apple.id + '">' + 'Fall it' + '</a></td>' +
+                '<td>' + apple.fallenAt + '</td>' +
+                '<td>' + detectAppleState(apple.state) + '</td>' +
+                '<td>' + 'apple.eatIt' + '</td>' +
+                '<td>' + apple.eatenPercent + '</td>' +
+                '<td><a href="javascript:void(0);" class="dropApple" data-id="' + apple.id + '">' + 'Drop it' + '</a></td>' +
+                '</tr>');
+        }
+    }
+}();
+
+const AppleColors = function () {
+    'use strict';
+    let colors = [];
+    return {
+        load: function (appleColors) {
+            colors = [];
+            $.each(appleColors, function (index, color) {
+                colors[color.id] = color.color;
+            });
+        },
+        /**
+         * @param id {number}
+         * @returns {string}
+         */
+        get: function (id) {
+            if (colors[id]) {
+                return colors[id];
+            }
+            return '';
+        }
+    };
+}();
+
+$(document).ready(function () {
     'use strict';
 
-    let list = $('#apples-list');
-    list.append('<tr>' +
-        '<td>id</td>' +
-        '<td>color</td>' +
-        '<td>Fallen</td>' +
-        '<td>Rotten</td>' +
-        '<td>Eat it for %</td>' +
-        '<td>Eaten %</td>' +
-        '<td>Drop it</td>' +
-        '</tr>');
+    $('a#createApples').on('click', function () {
+        $.ajax({
+            url: 'http://api-apples.local/apples/create',
+            headers: {
+                'Authorization': 'Bearer ' + AccessToken,
+                'Content-Type': 'application/json'
+            },
+            method: 'GET',
+            dataType: 'json',
+            data: '',
+            success: function (data) {
+            }
+        });
+    });
+
+    $('#applesTable').on('click', 'a.fallApple', function () {
+        console.log('FALL');
+        let id = $(this).data('id');
+        console.log(id);
+    });
+
+    $('#applesTable').on('click', 'a.dropApple', function () {
+        console.log('DROP');
+        let id = $(this).data('id');
+        console.log(id);
+    });
+
+    $('#applesTable').on('click', 'a.eatApple', function () {
+        console.log('DROP');
+        let id = $(this).data('id');
+        console.log(id);
+    });
+
+    ////////////////// LOAD TABLE /////////////////
 
     $.ajax({
         url: 'http://api-apples.local/apples/list',
         headers: {
-            'Authorization':'Bearer ' + AccessToken,
-            'Content-Type':'application/json'
+            'Authorization': 'Bearer ' + AccessToken,
+            'Content-Type': 'application/json'
         },
         method: 'GET',
         dataType: 'json',
         data: '',
-        success: function(data){
-            console.log(data);
+        success: function (data) {
+            let apples = data.data.apples,
+                appleColors = data.data.appleColors,
+                freshDuration = data.data.freshDuration; // hours
+
+            AppleColors.load(appleColors);
+
+            $.each(apples, function (index, apple) {
+                apple.color = AppleColors.get(apple.colorId);
+                apple.state = null;
+
+                if (apple.fallenAt) {
+                    // fallenAt {string} Date&Time ISO 8601
+                    apple.state = calculateFresh(new Date(apple.fallenAt), freshDuration);
+                }
+
+                // console.log(apple);
+                AppleList.load(apple);
+            });
         }
     });
 });
